ba7cfc31738b9ac1586566b1ccfea6e358a0347d
RoiAlv <Roi040205@gmail.com>
2025-03-07 13:12:24 +0100

    porfa

--- vite.config.ts
+++ vite.config.ts
@@ -9,15 +9,16 @@ export default defineConfig({
     tailwindcss(),
   ],
   server: {
-    port: process.env.PORT ? Number(process.env.PORT) : 5173,  // Convertimos el valor a un número
+    port: process.env.PORT ? Number(process.env.PORT) : 5173,  // Convertir process.env.PORT a un número
     host: '0.0.0.0',  // Asegura que el servidor sea accesible desde todas las interfaces
     hmr: true,  // Habilitar HMR si es necesario en desarrollo
+    allowedHosts: ['empleatufront.onrender.com'],  // Agregar tu dominio aquí
   },
   build: {
     outDir: 'dist',  // Configura la carpeta de salida para la construcción
   },
   preview: {
-    port: process.env.PORT ? Number(process.env.PORT) : 4173,  // Convertimos el valor a un número
+    port: process.env.PORT ? Number(process.env.PORT) : 4173,  // Convertir process.env.PORT a un número
     host: '0.0.0.0',  // Hacer que la vista previa sea accesible desde todas las interfaces
   },
 });
